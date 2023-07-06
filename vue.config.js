d57a469ac591b046a63dfcfb438804a18cb71e16
Félix Blanco <fexblanco@gmail.com>
2023-07-06 23:30:01 +0200

    .env handler

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/vue-production/'
-})
+  publicPath: process.env.VUE_APP_BASE_URL,
+});
