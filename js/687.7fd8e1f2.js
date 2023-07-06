81b4d7d48acb9a4e32c351ff7cc262f5793de152
Félix Blanco <fexblanco@gmail.com>
2023-07-07 01:44:13 +0200

    Updates

--- js/687.7fd8e1f2.js
+++ js/687.7fd8e1f2.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkvue_production"]=self["webpackChunkvue_production"]||[]).push([[687],{1687:function(u,n,e){e.r(n),e.d(n,{default:function(){return f}});var t=function(){var u=this,n=u._self._c;return n("p",[u._v("Home")])},r=[],l={name:"AHome"},o=l,c=e(1001),s=(0,c.Z)(o,t,r,!1,null,null,null),f=s.exports}}]);
-//# sourceMappingURL=687.d61cf59c.js.map
+//# sourceMappingURL=687.7fd8e1f2.js.map
--- js/891.ef487c58.js
+++ js/891.ef487c58.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkvue_production"]=self["webpackChunkvue_production"]||[]).push([[891],{891:function(u,n,e){e.r(n),e.d(n,{default:function(){return i}});var s=function(){var u=this,n=u._self._c;return n("p",[u._v("Quienes somos")])},o=[],t={name:"QuienesSomos"},r=t,l=e(1001),c=(0,l.Z)(r,s,o,!1,null,null,null),i=c.exports}}]);
-//# sourceMappingURL=891.18cbe0e3.js.map
+//# sourceMappingURL=891.ef487c58.js.map
